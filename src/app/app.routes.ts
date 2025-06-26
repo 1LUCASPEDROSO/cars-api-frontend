ddb128fc8c158d7760f09a0dfb6443fd8e66bafb
lucas pedroso <pedrosolucas1745@gmail.com>
2025-06-25 21:55:37 -0300

    refactor: create routes for list-model and form-model

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,15 @@
 import { Routes } from '@angular/router';
 import { ListBrandComponent } from './pages/list-brand/list-brand.component';
 import { FormBrandComponent } from './pages/form-brand/form-brand.component';
+import { ListModelComponent } from './pages/list-model/list-model.component';
+import { FormModelComponent } from './pages/form-model/form-model.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'list-brand', pathMatch: 'full' },
     {path:"list-brand",component: ListBrandComponent},
+    {path:"list-model",component: ListModelComponent},
     { path:'form-brand', component: FormBrandComponent },
+    { path:'form-model', component: FormModelComponent },
     { path:'form-brand/:id', component: FormBrandComponent },
+    { path:'form-model/:id', component: FormModelComponent },
 ];
