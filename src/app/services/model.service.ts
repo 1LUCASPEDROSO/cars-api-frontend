50ad1ada6718700b567f4672d996a137e5ff39db
lucas pedroso <pedrosolucas1745@gmail.com>
2025-06-25 21:48:31 -0300

    feat: create modelServices

---
+++ src/app/services/model.service.ts
@@ -0,0 +1,32 @@
+import { inject, Injectable } from '@angular/core';
+import { Model } from '../models/model';
+import { environment } from '../../environments/environment';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ModelService {
+apiUrl: string = environment.apiUrl
+  http = inject(HttpClient)
+  constructor() { }
+
+  getAllmModels(){
+    return this.http.get<Model[]>(this.apiUrl+"api/models")
+  }
+  getModelById(id: number){
+    return this.http.get<Model>(this.apiUrl+"api/models/id/"+id)
+  }
+  getModelByName(name: string){
+    return this.http.get<Model>(this.apiUrl+"api/models/name/"+name)
+  }
+  addModel(entity:Model){
+    return this.http.post(this.apiUrl+"api/models/",entity)
+  }
+  updateModel(entity:Model){
+    return this.http.put(this.apiUrl+"api/models/",entity)
+  }
+  deleteModel(id:number){
+    return this.http.delete(this.apiUrl+"api/models/"+id)
+  }
+}
