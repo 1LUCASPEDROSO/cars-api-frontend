08c7e420d4eb73d092002ffeadee257ba67ef1e4
lucas pedroso <pedrosolucas1745@gmail.com>
2025-07-07 19:55:30 -0300

    refactor: remove console.log and add updateModel method in save method

--- src/app/pages/form-model/form-model.component.ts
+++ src/app/pages/form-model/form-model.component.ts
@@ -1,10 +1,10 @@
 import { Component, inject } from '@angular/core';
 import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
-import { RouterModule,ActivatedRoute  } from '@angular/router';
+import { RouterModule, ActivatedRoute } from '@angular/router';
 import { OnInit } from '@angular/core';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
-import {MatSelectModule} from '@angular/material/select';
+import { MatSelectModule } from '@angular/material/select';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatError } from '@angular/material/form-field';
@@ -47,68 +47,74 @@ export class FormModelComponent implements OnInit {
 
   constructor(private fb: FormBuilder) {
     this.entityForm = this.fb.group({
-       name: ['', Validators.required],
+      name: ['', Validators.required],
       fipe_value: ['', Validators.required],
       brand_id: ['', Validators.required]
     });
   }
-  ngOnInit(){
+  ngOnInit() {
     this.entityId = this.route.snapshot.params["id"]
-    if (this.entityId){
+    if (this.entityId) {
       this.searchModel(this.entityId)
     }
-    else{
+    else {
       this.brandService.getAllBrands().subscribe(data => {
-      this.brands = data;
-    });
+        this.brands = data;
+      });
     }
-    }
-  searchModel(id: number){
+  }
+  searchModel(id: number) {
 
-    this.modelService.getModelById(id).subscribe((data:any) =>{
-      if(data == null){
-        console.log("erro")
-      }
-      this.brandService.getBrandById(data.brand_id).subscribe(brand =>{
+    this.modelService.getModelById(id).subscribe((data: any) => {
+      this.brandService.getBrandById(data.brand_id).subscribe(brand => {
         this.brands = [brand]
       })
       this.entityForm.controls['name'].setValue(data.name.toLowerCase())
       this.entityForm.controls['fipe_value'].setValue(data.fipe_value)
       this.entityForm.controls['brand_id'].setValue(data.brand_id)
-      this.entityForm.get('brand_id')?.disable();
+      this.entityForm.get('brand_id')?.disable()
     })
   }
   Save() {
-  if (this.entityForm.valid) {
-    const modelName = this.entityForm.value.name.trim().toLowerCase();
-    const ModelData1: Model = { name: modelName, fipe_value: this.entityForm.value.fipe_value,brand_id: this.entityForm.value.brand_id };
-    console.log(ModelData1) 
-    this.modelService.getModelByName(modelName).subscribe({
-      next: (existingModel) => {
-        console.log("dentro da req" + modelName)
-        if (existingModel) {
-          this.ngx.warning("Esse modelo já está registrado.");
-        } else {
-          const ModelData: Model = { name: modelName, fipe_value: this.entityForm.value.fipe_value,brand_id: this.entityForm.value.brand_id };
-          this.modelService.addModel(ModelData).subscribe({
-            next: () => {
-              this.ngx.success("Modelo registrado com sucesso");
-            },
-            error: (error) => {
-              this.ngx.error(error?.message || "Erro ao registrar modelo");
-            }
-          });
+    const raw = this.entityForm.getRawValue();
+    if (this.entityId && this.entityId > 0) {
+      const updateModelData: Model = { id: this.entityId, name: this.entityForm.value.name.toLowerCase(), fipe_value: this.entityForm.value.fipe_value, brand_id: raw.brand_id };
+      this.modelService.updateModel(updateModelData).subscribe({
+        next: () => {
+          this.ngx.success("modelo editado com sucesso");
+        },
+        error: (error) => {
+          this.ngx.error(error?.message || "Erro ao editar modelo");
         }
-      },
-      error: () => {
-        this.ngx.error("Erro ao verificar se modelo já existe");
-      }
-    });
-  } 
- else {
-    this.entityForm.markAllAsTouched();
+      });
+    }
+    else if (this.entityForm.valid) {
+      const modelName = this.entityForm.value.name.trim().toLowerCase();
+      this.modelService.getModelByName(modelName).subscribe({
+        next: (existingModel) => {
+          if (existingModel) {
+            this.ngx.warning("Esse modelo já está registrado.");
+          } else {
+            const ModelData: Model = { name: modelName, fipe_value: this.entityForm.value.fipe_value, brand_id: this.entityForm.value.brand_id };
+            this.modelService.addModel(ModelData).subscribe({
+              next: () => {
+                this.ngx.success("Modelo registrado com sucesso");
+              },
+              error: (error) => {
+                this.ngx.error(error?.message || "Erro ao registrar modelo");
+              }
+            });
+          }
+        },
+        error: () => {
+          this.ngx.error("Erro ao verificar se modelo já existe");
+        }
+      });
+    }
+    else {
+      this.entityForm.markAllAsTouched();
+    }
   }
-}
-  
-  
+
+
 }
