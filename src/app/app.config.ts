44543aee4f05b039e05d2b17c0f0dde47625c2df
lucas pedroso <pedrosolucas1745@gmail.com>
2025-08-18 21:30:53 -0300

    refactor: injecrt auth interceptor to use in requests

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,14 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { routes } from './app.routes'; // importa a constante 'routes', não 'Routes'
 import { provideToastr  } from 'ngx-toastr';
 import { provideAnimations } from '@angular/platform-browser/animations';
+import { authInterceptor } from './interceptors/auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideHttpClient(),
+    provideHttpClient(withInterceptors([authInterceptor])),
     provideRouter(routes),
     provideAnimations(),
     provideToastr() // aqui usa a constante 'routes'
